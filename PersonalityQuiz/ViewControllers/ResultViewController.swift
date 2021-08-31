edd1e208f5c3d2e5549b2fa55d223064cd53a84a
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2021-08-31 18:27:15 +0400

    Add logic to result view controller, hide backbitten in result view controller

--- PersonalityQuiz/ViewControllers/ResultViewController.swift
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -9,13 +9,45 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
-    // 1. Передать сюда массив с ответами
-    // 2. Определить наиболее часто встречающийся тип животного
-    // 3. Отобразить результат в соответствии с этим животным
-    // 4. Избавиться от кнопки возврата на предыдущий экран
-
+    // MARK: - IBOutlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    // MARK: - Public properties
+    var answers: [Answer]!
+    
+    // MARK: - Private properties
+    private var popularAnimal: Animal = .dog
+    
+    // MARK: - Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.setHidesBackButton(true, animated: false)
+        updateUI()
     }
+    
+    // MARK: - Private methods
+    private func defineMostPopularAnimal() {
+        var countAnswerDictionary: [Animal: Int] = [:]
+        
+        for answer in answers {
+            if let counter = countAnswerDictionary[answer.animal] {
+                countAnswerDictionary[answer.animal] = counter + 1
+            } else {
+                countAnswerDictionary[answer.animal] = 1
+            }
+        }
+        
+        let sortedAnswers = countAnswerDictionary.sorted { $0.1 > $1.1 }
+        
+        guard let animal = sortedAnswers.first?.key else { return }
+        popularAnimal = animal
+    }
+    
+    private func updateUI() {
+        defineMostPopularAnimal()
+        titleLabel.text = "Вы - \(popularAnimal.rawValue)!"
+        descriptionLabel.text = popularAnimal.definition
+    }
+    
 }
